162d4fa925ebbea622b82f9ff0d646089d5c9706
hong bo <hongbo@hongmatoMac-mini.local>
2012-11-11 10:06:11 +0800

    build project base

---
+++ ShareArch/ShareArch/WhtooAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WhtooAppDelegate.h
+//  ShareArch
+//
+//  Created by hong bo on 12-11-11.
+//  Copyright (c) 2012年 OpenX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WhtooAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ShareArch/ShareArch/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ShareArch
+//
+//  Created by hong bo on 12-11-11.
+//  Copyright (c) 2012年 OpenX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WhtooAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WhtooAppDelegate class]));
+    }
+}
